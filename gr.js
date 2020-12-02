4a056b59d4011630013852ab7dfe288ee54a3e4a
gve123 <74577907+gve123@users.noreply.github.com>
2020-12-02 13:55:20 -0500

    Add files via upload

---
+++ gr.js
@@ -0,0 +1,5 @@
+
+document.getElementById("imgr6").src="img/g6.png"
+document.getElementById("a6").href = "https://www.facebook.com/groups/177546719885392/";
+document.getElementById("a6").innerHTML = "(g6) Herramientas Caseras"
+
