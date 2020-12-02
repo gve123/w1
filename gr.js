7e19272e9f5257dc6624584d14f24374c4355fd5
gve123 <74577907+gve123@users.noreply.github.com>
2020-12-02 15:56:59 -0500

    Add files via upload

--- gr.js
+++ gr.js
@@ -1,33 +1,33 @@
 
 
-document.getElementById("imgr1").src="img/g1.png"
+document.getElementById("imgr1").src="img/g1.png";
 document.getElementById("a1").href = "https://www.facebook.com/groups/178318236599507/";
 document.getElementById("a1").innerHTML = "(g1) Inventos Caseros";
 
 
-document.getElementById("imgr2").src="img/g2.png"
+document.getElementById("imgr2").src="img/g2.png";
 document.getElementById("a2").href = "https://www.facebook.com/groups/238196284129161/";
 document.getElementById("a2").innerHTML = "(g2) Electricidad Automotriz";
 
 
-document.getElementById("imgr3").src="img/g3.png"
+document.getElementById("imgr3").src="img/g3.png";
 document.getElementById("a3").href = "https://www.facebook.com/groups/515415822673569/";
 document.getElementById("a3").innerHTML = "(g3) Inventos Caseros con Materiales Reciclados";
 
 
-document.getElementById("imgr4").src="img/g4.png"
+document.getElementById("imgr4").src="img/g4.png";
 document.getElementById("a4").href = "https://www.facebook.com/groups/2977701305597766/";
 document.getElementById("a4").innerHTML = "(g4) Reparaciones Caseras";
 
 
 
-document.getElementById("imgr5").src="img/g5.png"
+document.getElementById("imgr5").src="img/g5.png";
 document.getElementById("a5").href = "https://www.facebook.com/groups/2485289741730986/";
 document.getElementById("a5").innerHTML = "(g5) Electricidad Basica";
 
 
 
-document.getElementById("imgr6").src="img/g6.png"
+document.getElementById("imgr6").src="img/g6.png";
 document.getElementById("a6").href = "https://www.facebook.com/groups/177546719885392/";
 document.getElementById("a6").innerHTML = "(g6) Herramientas Caseras";
 
