0d8b75115f3e87878bf1086f0803b8e7d616b298
gve123 <74577907+gve123@users.noreply.github.com>
2020-12-02 18:51:36 -0500

    Add files via upload

--- gr.js
+++ gr.js
@@ -40,3 +40,148 @@ document.getElementById("imgr6").src="img/g6.png";
 document.getElementById("a6").href = "https://www.facebook.com/groups/177546719885392/";
 document.getElementById("a6").innerHTML = "(g6) Herramientas Caseras";
 }
+
+else if (n==7)
+{
+document.getElementById("imgr7").src="img/g7.png";
+document.getElementById("a7").href = "https://www.facebook.com/groups/834602917003573/";
+document.getElementById("a7").innerHTML = "(g7) El Mundo de la Electronica";
+}
+
+else if (n==8)
+{
+document.getElementById("imgr8").src="img/g8.png";
+document.getElementById("a8").href = "https://www.facebook.com/groups/642275522987646/";
+document.getElementById("a8").innerHTML = "(g8) Diagramas y Circuitos Electronicos";
+}
+
+else if (n==9)
+{
+document.getElementById("imgr9").src="img/g9.png";
+document.getElementById("a9").href = "https://www.facebook.com/groups/2511258972430608/";
+document.getElementById("a9").innerHTML = "(g9) Proyectos de Electronica";
+}
+
+else if (n==10)
+{
+document.getElementById("imgr10").src="img/g10.png";
+document.getElementById("a10").href = "https://www.facebook.com/groups/216568049771870/";
+document.getElementById("a10").innerHTML = "(g10) Reparacion de Equipos Electricos";
+}
+
+else if (n==11)
+{
+document.getElementById("imgr11").src="img/g11.png";
+document.getElementById("a11").href = "https://www.facebook.com/groups/1120240875010811/";
+document.getElementById("a11").innerHTML = "(g11) Electricidad Residencial";
+}
+
+else if (n==12)
+{
+document.getElementById("imgr12").src="img/g12.png";
+document.getElementById("a12").href = "https://www.facebook.com/groups/236708151053211/";
+document.getElementById("a12").innerHTML = "(g12) Inventos Ciencia y Tecnologia";
+}
+
+else if (n==13)
+{
+document.getElementById("imgr13").src="img/g13.png";
+document.getElementById("a13").href = "https://www.facebook.com/groups/282152645582813/";
+document.getElementById("a13").innerHTML = "(g13) Inventos y Curiosidades";
+}
+
+else if (n==14)
+{
+document.getElementById("imgr14").src="img/g14.png";
+document.getElementById("a14").href = "https://www.facebook.com/groups/395502904546229/";
+document.getElementById("a14").innerHTML = "(g14) manuales de Electricidad, electrónica, informática y más";
+}
+
+else if (n==15)
+{
+document.getElementById("imgr15").src="img/g15.png";
+document.getElementById("a15").href = "https://www.facebook.com/groups/113269189396904/";
+document.getElementById("a15").innerHTML = "(g15) EXPERIMENTOS ELECTRONICOS-TRABAJOS TECNICOS Y VENTAS DE INSUMOS";
+}
+
+else if (n==16)
+{
+document.getElementById("imgr16").src="img/g16.png";
+document.getElementById("a16").href = "https://www.facebook.com/groups/todoinventostvElGrupoDeTodos";
+document.getElementById("a16").innerHTML = "(g16) Grupo Todo Inventos TV, el grupo de TODOS";
+}
+
+else if (n==17)
+{
+document.getElementById("imgr17").src="img/g17.png";
+document.getElementById("a17").href = "https://www.facebook.com/groups/136638303381850/";
+document.getElementById("a17").innerHTML = "(g17) Electricista alta y baja tensión";
+}
+
+else if (n==18)
+{
+document.getElementById("imgr18").src="img/g18.png";
+document.getElementById("a18").href = "https://www.facebook.com/groups/393322834166100/";
+document.getElementById("a18").innerHTML = "(g18) Electricidad, Electrónica y Refrigeración.";
+}
+
+else if (n==19)
+{
+document.getElementById("imgr19").src="img/g19.png";
+document.getElementById("a19").href = "https://www.facebook.com/groups/saberelectronica/";
+document.getElementById("a19").innerHTML = "(g19) Saber Electronica!!!";
+}
+
+else if (n==20)
+{
+document.getElementById("imgr20").src="img/g20.png";
+document.getElementById("a20").href = "https://www.facebook.com/groups/523695104458806/";
+document.getElementById("a20").innerHTML = "(g20) INVENTOS CASEROS Y UTILES IDEAS, compartir ideas y buscar soluciones..";
+}
+
+else if (n==21)
+{
+document.getElementById("imgr21").src="img/g21.png";
+document.getElementById("a21").href = "https://www.facebook.com/groups/461380397735693/";
+document.getElementById("a21").innerHTML = "(g21) Aprenda Electricidad Básica Gratis";
+}
+
+else if (n==22)
+{
+document.getElementById("imgr22").src="img/g22.png";
+document.getElementById("a22").href = "https://www.facebook.com/groups/MONGUIBOYRDELECTRONICA/";
+document.getElementById("a22").innerHTML = "(g22) EL MUNDO DE LA ELECTRÓNICA";
+}
+
+else if (n==23)
+{
+document.getElementById("imgr23").src="img/g23.png";
+document.getElementById("a23").href = "https://www.facebook.com/groups/electronicabasicachile";
+document.getElementById("a23").innerHTML = "(g23) Diagramas y Proyectos Electrónicos";
+}
+
+else if (n==24)
+{
+document.getElementById("imgr24").src="img/g24.png";
+document.getElementById("a24").href = "https://www.facebook.com/groups/658028554281343";
+document.getElementById("a24").innerHTML = "(g24) Electricidad y Electrónica Automotriz";
+}
+
+else if (n==25)
+{
+document.getElementById("imgr25").src="img/g25.png";
+document.getElementById("a25").href = "https://www.facebook.com/groups/1242246439187581";
+document.getElementById("a25").innerHTML = "(g25) Tecnicos En Electricidad y Automatización";
+}
+
+else if (n==26)
+{
+document.getElementById("imgr26").src="img/g26.png";
+document.getElementById("a26").href = "https://www.facebook.com/groups/JorgePolania";
+document.getElementById("a26").innerHTML = "(g26) Ingeniería Electrónica";
+}
+
+
+
+
+
