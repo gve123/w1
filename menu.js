275effc7d8d9fea105a065f0494e34e22bbd7344
gve123 <74577907+gve123@users.noreply.github.com>
2020-12-02 20:06:49 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,7 +2,7 @@
   document.write('<li><a href="https://gve123.github.io/w1/">Inicio</a>');	
   document.write('<li><a href="#">grupos</a><ul>');
 
-for (var k = 1 ; k <= 35; k++) {
+for (var k = 1 ; k <= 26; k++) {
 
 	document.write('<li><a href="https://gve123.github.io/w1/g'+k+'.html">grupo'+k+'</a></li>');
 }	
